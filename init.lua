777cb38ee6c2e670dbcb7d0683f7f52886b7f792
v-rob <31123645+v-rob@users.noreply.github.com>
2018-03-24 16:37:47 -0700

    Add everything

---
+++ init.lua
@@ -0,0 +1,15 @@
+minetest.register_node("bridger:scaffolding", {
+	description = "Scaffolding",
+	drawtype = "glasslike_framed_optional",
+	tiles = {"bridges_scaffolding.png", "bridges_scaffolding_detail.png"},
+	paramtype = "light",
+	paramtype2 = "glasslikeliquidlevel",
+	sunlight_propagates = true,
+	walkable = false,
+	climbable = true,
+	groups = {cracky = 3, oddly_breakable_by_hand = 3, dig_immediate = 3},
+	sounds = default.node_sound_wood_defaults(),
+})
+
+dofile(minetest.get_modpath("bridger").."/nodes.lua")
+dofile(minetest.get_modpath("bridger").."/crafts.lua")
